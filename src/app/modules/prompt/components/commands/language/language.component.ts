fc73343e69944b91bbd4ad0c9bd33e4f761cc4c7
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-05-30 21:04:40 +0200

    Improved language command

--- src/app/modules/prompt/components/commands/language/language.component.ts
+++ src/app/modules/prompt/components/commands/language/language.component.ts
@@ -22,15 +22,11 @@ export class LanguageComponent implements OnInit {
 
   ngOnInit() {
     if (this.values.length === 0) {
-      console.log('Error');
-      this.terminal.sendError('language', this.translate.instant('prompt.commands.language.missing_value'));
       return;
     }
     let language = this.values[0];
 
     if (!this.translate.getLangs().includes(language)) {
-      console.log('Error');
-      this.terminal.sendError('language', this.translate.instant('prompt.commands.language.not_found'));
       return;
     }
     this.translate.use(language);
